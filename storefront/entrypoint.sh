4bce9c9cf2a1fa6e901a41a35642beb6613fcece
Rob <rob.p.tec@gmail.com>
2023-04-05 16:55:34 +0100

    remix

--- storefront/entrypoint.sh
+++ storefront/entrypoint.sh
@@ -2,8 +2,8 @@
 
 npm install
 
-echo "VENDURE_API_URL=${SERVER_API_HOST}:${SERVER_API_PORT}/${SERVER_API_PATH}" > .env
+echo "VENDURE_API_URL=${SERVER_API_HOST}:${SERVER_API_PORT}/${SERVER_API_PATH}" > /usr/src/app/.env
 
-echo "NODE_ENV=production" >> .env
+echo "NODE_ENV=production" >> /usr/src/app/.env
 
 npm run dev
