be8ce6dcc1946c8549bde19c0534555c20282f03
Rob <rob.p.tec@gmail.com>
2023-04-08 23:52:54 +0100

    env vars

--- storefront/entrypoint.sh
+++ storefront/entrypoint.sh
@@ -5,5 +5,6 @@ npm install
 echo ">>> Seeding env vars to .env file"
 echo "VENDURE_API_URL=${SERVER_API_HOST}:${SERVER_API_PORT}/${SERVER_API_PATH}" > /usr/src/app/.env
 echo "NODE_ENV=production" >> /usr/src/app/.env
+echo "STRIPE_PUBLISHABLE_KEY=${STRIPE_PUBLISHABLE_KEY}" >> /usr/src/app/.env
 
 npm run dev
