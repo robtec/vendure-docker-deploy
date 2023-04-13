7afe539d85cfaf5ad1fec119f678f7fec0c65679
Rob <robtec@users.noreply.github.com>
2023-04-13 14:39:09 +0100

    Update vendure-config.ts
    updated verify endpoint

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -80,7 +80,7 @@ export const config: VendureConfig = {
             templatePath: path.join(__dirname, '../static/email/templates'),
             globalTemplateVars: {
                 fromAddress: process.env.NO_REPLY_EMAIL,
-                verifyEmailAddressUrl: 'https://' + process.env.PRIMARY_DOMAIN + '/account/verify',
+                verifyEmailAddressUrl: 'https://' + process.env.PRIMARY_DOMAIN + '/verify',
                 passwordResetUrl: 'https://' + process.env.PRIMARY_DOMAIN + '/account/reset-password',
                 changeEmailAddressUrl: 'https://' + process.env.PRIMARY_DOMAIN + '/account/change-email-address'
             },
