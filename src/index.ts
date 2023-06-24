b18157134c90873023d0fa21cef3838f7d7c2afb
Rob <robtec@users.noreply.github.com>
2023-06-24 14:09:01 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { bootstrap, runMigrations } from '@vendure/core';
 import { config } from './vendure-config';
 import { populateOnFirstRun } from './populate';
 
-runMigrations(config)
+populateOnFirstRun(config)
+    .then(() => runMigrations(config))
     .then(() => bootstrap(config))
     .catch(err => {
         console.log(err);
